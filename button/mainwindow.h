82dba3d66d5fdafcdeadd996b388ce5981623553
zoreba <bee837@naver.com>
2018-09-20 12:20:28 +0900

    sample

--- button/mainwindow.h
+++ button/mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QPushButton>
 #include <QFocusFrame>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -17,16 +18,19 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-    QPushButton *btn[3];
-    int chk = 0;
+    QPushButton *btn[4];
+    int chk[3] = {0,0,0};
 
 private:
     Ui::MainWindow *ui;
+    QTimer timer;
+    QPalette p=palette();
+    void btn_start();
 
 private slots:
-    void btn_click();
-    void btn_pressed();
-    void btn_released();
+    void btn_click1();
+    void btn_click2();
+    void btn_status();
 };
 
 #endif // MAINWINDOW_H
